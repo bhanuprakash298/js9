ed22dc74ba75adf56c2710e71e5779b0a1a1508c
Bhanu Prakash <bhanuprakashmoram14@example.com>
2026-07-08 22:51:27 +0530

    Completed JavaScript Task 7

--- script.js
+++ script.js
@@ -1,285 +1,97 @@
 
-// Task 1 ==Employee Bonus (Spread Operator)
+
+// Task =1  Employee Salary Dashboard
 
 
-let emp={
-    name : "Bhanu",
-    salary : 25000
+let employees = [
+  { name: "Bhanu", salary: 45000 },
+  { name: "Ravi", salary: 60000 },
+  { name: "Anjali", salary: 120000 },
+  { name: "Kiran", salary: 55000 }
+];
+let employeeNames = employees.map(emp => emp.name);
+console.log("Employee Names:", employeeNames);
+
+// sal greater than 50000
+let empsalname=employees.filter(emp =>emp.salary >50000);
+console.log(empsalname);
+
+let firstHighSalary = employees.find(emp => emp.salary > 100000);
+console.log("first emp salary with 100000 :",firstHighSalary);
+
+let findtotalexp=employees.reduce((total,emp)=>total+emp.salary,0);
+console.log("Total expense :",findtotalexp);
+
+
+// task =2  E-Commerce Cart Management
+
+let cart1=["milk","powder","bag"];
+let cart2=["vegetables","fruits","non-veg"];
+
+let combinedprod=[...cart1,...cart2];
+console.log("Merged products :",combinedprod);
+
+let addnewprod=[...combinedprod,"mutton"];
+console.log("after adding new product :",addnewprod);
+
+function  checkout(...products)
+{
+    console.log("prodcuts :",products);
+    console.log("total products :",products.length);
+}
+checkout("monitor","mobile","keyboard","mouse");
+
+
+// task =3 Student Report Card
+
+let student={
+    name :"bhanu",dept :"cse",marks:[55,69,85,98]
 };
-let newemp={
-    ...emp,
-    bonous : 5000,
-    depart : "Software"
-};
 
-console.log(newemp);
-
-
-// Task 2 == Shopping Cart (Rest Operator)
-
-function calculateBill(... items)
-{
-    let total=0;
-    for(let i=0; i<items.length ; i++)
-    {
-        total +=items[i];
-    }
-    console.log("items",items.length);
-    console.log("Total :"+total);
-}
-calculateBill(10,30,40,50);
-
-
-// Task 3 == Student Details (Object Destructuring) 
-
-let student = {
-  name:"Rahul",
-  age:22,
-  course:"MERN",
-  city:"Chennai"
-};
-
-let {name,age,course,city}=student;
-console.log(name);
-console.log(age);
-console.log(course);
-console.log(city);
-
-// Task 4 == Online Food Order 
-
-let food= ["pizza", "Burger" , "Fries"];
-food.push("coke");
-food.push("Ice cream");
-
-food.splice(2,1);
-console.log(food);
-
-// Task 5 == Bank Transactions 
-
-let transactions =[1000,5500,9510,15000,20000];
-transactions.shift();
-transactions.unshift(500);
-console.log(transactions);
-
-// Task 6 ==  Company Departments
-
-let dept1=["HR","Sales"];
-let dept2=["Developer","Testing"];
-
-let finaldept=dept1.concat(dept2);
-console.log(finaldept);
-
-// Task 7 == Product Search
-
-let products=[
-"Laptop",
-"Mouse",
-"Keyboard",
-"Monitor"];
-
-if (products.includes("Mouse"))
-{
-     console.log("Available");
-     
-}
-else
-{
-    console.log("Not available");
-}
-
-// Task =8 User login - callback
-
-function dashboard() {
-    console.log("Dashboard Opened");
-}
-
-function loginn(callback) {
-    console.log("Login Success");
-    callback();
-}
-
-loginn(dashboard);
-
-// Task =9 Coupon Generator (Generator Function)
-
-function * coupons()
-{
-   yield "WELCOME10",
-   yield "save10",
-   yield "mega50"
-   yield "free100"
-}
-
-let coupon=coupons();
-console.log(coupon.next().value);
-console.log(coupon.next().value);
-console.log(coupon.next().value);
-console.log(coupon.next().value);
-
-// Task = 10 course registartion 
-
-function coursee(a)
-{
-    return function(b)
-    {
-        return function(c)
-        {
-            console.log("course :",a);
-            console.log("Batch :",b);
-            console.log("Room :",c);
-        }
-    }
-}
-coursee("JavaScript")("Morning")("Batch-36")
-
-
-// Task = 11 Movie collection
-
-let movies=[
-"Leo",
-"GOAT",
-"Dragon",
-"Retro",
-"Coolie"
-];
-
-let print=movies.slice(2,4);
-console.log(print);
-
-// Task = 12 Employee ids 
-
-let ids=[101,102,103,104,105];
-
-// ids.splice(2,1,501);
-// ids.splice(3,1,502);
-ids.splice(2,2,501,502)
-console.log(ids);
-
-// Task =13  Sort product prices 
-
-let price=[
-5000,
-250,
-700,
-12000,
-50
-];
-price.sort((a,b) =>a-b);
-console.log(price);
-
-
-// Task =14 -- Nested categories
-
-let data=[
-"Electronics",
-[
-"Mobiles",
-[
-"Samsung",
-"Apple"
-]
-]
-];
-
-let [a,[b,[c1,c2]]] =data;
-console.log(a);
-console.log(b);
-console.log(c1);
-console.log(c2);
-
-
-// Task =15 - Company team
-
-let developers=[
-"John",
-"David"
-];
-
-let designers=
-[ ...developers,
-"Ram",
-"Kiran"
-];
-
-console.log(designers);
-
-// Task ==16 Return function
-
-function calculateSalary(salary,bonous)
-{
-    return salary+bonous;
-}
-let totalsal=calculateSalary(25000,5000);
-console.log(totalsal);
-
-// Task ==17 scope checking
-
-if (true) {
-    var x = "I am var";
-    let y = "I am let";
-    const z = "I am const";
-}
-
-console.log(x);
-//console.log(y);
-//console.log(z);
-
-// answer : here we are getting two errors they are y, z beacause let and const are block scoped and give errors outside
-
-// Task = 18 customer orders
-
-let orders=[
-"Pizza",
-"Burger",
-"Pizza",
-"Sandwich",
-"Pizza"
-];
-console.log("First Index :", orders.indexOf("Pizza"));
-console.log("Last Index :", orders.lastIndexOf("Pizza"))
-
-// Task =19 Flatten Product Categories
-
-let items=[
-1,
-2,
-[
-3,
-4,
-[
-5,
-6
-]
-]
-] ;
-
-let flateditem=items.flat(2);
-console.log(flateditem);
-
-// task 20==Mini Shopping Cart Challenge
-
-let cart=["Mouse","Keyboard"];
-cart.push("Monitor");
-cart.unshift("laptop");
-cart.splice(2,1);
-if(cart.includes("Mouse"))
-{
-    console.log("exist");
-    
-}
-else
-{
-    console.log("not there");
-}
-let accessories=[ ...cart,"Webcam","Speaker"];
-console.log(accessories);
-
-
-
-
-
-
-
+let {name,dept,marks}=student;
+let{sub1,sub2,sub3}=marks;
+
+let totalMarks = marks.reduce((sum, mark) => sum + mark, 0);
+let averageMarks = totalMarks / marks.length;
+console.log("Student Name:", name);
+console.log("Department:", dept);
+console.log("First Subject:", sub1);
+console.log("Second Subject:", sub2);
+console.log("Third Subject:", sub3);
+console.log("total marks :",totalMarks);
+console.log("average marks :",averageMarks);
+
+
+//task =4 product search system
+
+let searchText = "   Laptop   ";
+let trimmed = searchText.trim();
+console.log("Trimmed:", trimmed);
+
+let lower = trimmed.toLowerCase();
+console.log("Lowercase:", lower);
+let product = "gaming laptop";
+console.log("Contains Search:", product.includes(lower));
+let highlighted = product.replace("laptop", "[LAPTOP]");
+console.log("Highlighted:", highlighted);
+
+// task =5 movie collection manager
+let movies = ["Bahubali", "RRR", "Pushpa"];
+movies.push("Salaar");
+console.log("After Push:", movies);
+movies.pop();
+console.log("After Pop:", movies);
+movies.shift();
+console.log("After Shift:", movies);
+movies.unshift("Kalki");
+console.log("After Unshift:", movies);
+
+movies.splice(1, 1, "Devara");
+console.log("After Splice:", movies);
+let ratings = [4.2, 4.8, 3.9, 5.0, 4.5];
+ratings.sort((a, b) => b - a);
+console.log("Ratings Descending:", ratings);
+console.log("Movie Exists:", movies.includes("RRR"));
 
 
 
