f6bf2e2819accc4f3029b324908b6549aded4e2b
Bhanu Prakash <bhanuprakashmoram14@example.com>
2026-07-10 12:04:19 +0530

    Completed JavaScript Task 8

--- script.js
+++ script.js
@@ -1,97 +1,64 @@
 
 
-// Task =1  Employee Salary Dashboard
+// task =1 employee age calculator
 
+let data1=new Date();
+data1.setFullYear(2003);
+data1.setMonth(5);
+data1.setDate(25);
+console.log(data1.getFullYear());
 
-let employees = [
-  { name: "Bhanu", salary: 45000 },
-  { name: "Ravi", salary: 60000 },
-  { name: "Anjali", salary: 120000 },
-  { name: "Kiran", salary: 55000 }
-];
-let employeeNames = employees.map(emp => emp.name);
-console.log("Employee Names:", employeeNames);
+let curdate=new Date();
 
-// sal greater than 50000
-let empsalname=employees.filter(emp =>emp.salary >50000);
-console.log(empsalname);
+let age=curdate.getFullYear()-data1.getFullYear();
+console.log("Employee age ,",age,"years");
 
-let firstHighSalary = employees.find(emp => emp.salary > 100000);
-console.log("first emp salary with 100000 :",firstHighSalary);
+// task =2 online appointment scheduler
 
-let findtotalexp=employees.reduce((total,emp)=>total+emp.salary,0);
-console.log("Total expense :",findtotalexp);
+let date2=new Date();
+date2.setFullYear(2027);
+date2.setMonth(11);
+date2.setDate(15);
+date2.setHours(10);
+date2.setMinutes(30);
+console.log("appointment updated");
+console.log(date2.toLocaleString());
 
+// task =3  Multi-Country Meeting Time
 
-// task =2  E-Commerce Cart Management
+let data3=new Date();
+console.log("India time zone");
+console.log(data3.toLocaleString("en-us",{timeZone: "Asia/kolkata"}));
+console.log("New York Time:");
+console.log(data3.toLocaleString("en-US", {timeZone: "America/New_York"}));
+console.log("tokyo time zone:");
+console.log(data3.toLocaleString("en-us",{timeZone:"Asia/Tokyo"}));
 
-let cart1=["milk","powder","bag"];
-let cart2=["vegetables","fruits","non-veg"];
+// task =4  Product Warranty Expiry
 
-let combinedprod=[...cart1,...cart2];
-console.log("Merged products :",combinedprod);
+let today=new Date();
+console.log("purcahse date :",today);
+let expdate=new Date();
+expdate.setFullYear(2028);
+expdate.setMonth(6);
+expdate.setDate(9);
+console.log("expirey date :",expdate);
 
-let addnewprod=[...combinedprod,"mutton"];
-console.log("after adding new product :",addnewprod);
+// task =5  Digital Clock
 
-function  checkout(...products)
+setInterval(()=>
 {
-    console.log("prodcuts :",products);
-    console.log("total products :",products.length);
-}
-checkout("monitor","mobile","keyboard","mouse");
+    let clock=new Date();
+    console.log(clock.toLocaleTimeString());
+    
+},1000);
 
 
-// task =3 Student Report Card
-
-let student={
-    name :"bhanu",dept :"cse",marks:[55,69,85,98]
-};
-
-let {name,dept,marks}=student;
-let{sub1,sub2,sub3}=marks;
-
-let totalMarks = marks.reduce((sum, mark) => sum + mark, 0);
-let averageMarks = totalMarks / marks.length;
-console.log("Student Name:", name);
-console.log("Department:", dept);
-console.log("First Subject:", sub1);
-console.log("Second Subject:", sub2);
-console.log("Third Subject:", sub3);
-console.log("total marks :",totalMarks);
-console.log("average marks :",averageMarks);
 
 
-//task =4 product search system
 
-let searchText = "   Laptop   ";
-let trimmed = searchText.trim();
-console.log("Trimmed:", trimmed);
 
-let lower = trimmed.toLowerCase();
-console.log("Lowercase:", lower);
-let product = "gaming laptop";
-console.log("Contains Search:", product.includes(lower));
-let highlighted = product.replace("laptop", "[LAPTOP]");
-console.log("Highlighted:", highlighted);
 
-// task =5 movie collection manager
-let movies = ["Bahubali", "RRR", "Pushpa"];
-movies.push("Salaar");
-console.log("After Push:", movies);
-movies.pop();
-console.log("After Pop:", movies);
-movies.shift();
-console.log("After Shift:", movies);
-movies.unshift("Kalki");
-console.log("After Unshift:", movies);
-
-movies.splice(1, 1, "Devara");
-console.log("After Splice:", movies);
-let ratings = [4.2, 4.8, 3.9, 5.0, 4.5];
-ratings.sort((a, b) => b - a);
-console.log("Ratings Descending:", ratings);
-console.log("Movie Exists:", movies.includes("RRR"));
 
 
 
