2ed077cb388de9bcd50662ec9107724bb52ab909
Bhanu Prakash <bhanuprakashmoram14@example.com>
2026-07-15 07:30:44 +0530

    Completed JavaScript Day 1 Tasks

--- script.js
+++ script.js
@@ -1,209 +1,83 @@
+// Task --1 == employee name
 
-// API store cart project
+let empname = prompt("Enter Employee Name:");
+console.log(empname);
+document.writeln("Welcome " + empname + "<br><br>");
 
-let products = [];
-let cart = [];
-let productContainer = document.getElementById("products");
-let search = document.getElementById("search");
-fetch("https://fakestoreapi.com/products")
-.then(function(response)
+
+// Task 2 -=- Company Entry Confirmation
+
+let training = confirm("Are you ready to join today's JavaScript training?");
+console.log(training);
+
+
+// Task ==3 - Customer Greeting
+
+alert("Welcome to Stackly Solutions!");
+console.log("Customer entered the website.");
+
+
+// Task ==4 - Student Details
+
+let stuName = prompt("Enter Student Name:");
+let stuAge = prompt("Enter Student Age:");
+console.log("Student Name :", stuName);
+console.log("Student Age :", stuAge);
+
+// Task ==5 - Product Price
+
+let prodPrice = 500;
+prodrice = 750;
+console.log("Updated Price :", prodtPrice);
+
+
+// Task 6 - Login Validation
+let username = prompt("Enter Username:");
+if (username == "")
 {
-    return response.json();
-})
-.then(function(data)
-{
-    products = data;
-    displayProducts(products);
-
-});
-
-function displayProducts(items){
-    productContainer.innerHTML = "";
-    if(items.length===0)
-        {
-        productContainer.innerHTML=
-        "<h2 class='notFound'>No Products Found</h2>";
-        return;
-    }
-    items.forEach(function(product){
-     productContainer.innerHTML += `
-<div class="card">
-    <img
-        src="${product.image}"
-        class="productImage"
-        data-id="${product.id}"
-    >
-    <h3>${product.title}</h3>
-    <p>$${product.price}</p>
-    <button
-        class="cartBtn"
-        data-id="${product.id}"
-    >
-        add to cart
-    </button>
-
-</div>
-
-`;
-    });
+    console.warn("Warning: Username is empty.");
 }
 
-// task 1 : product search
-
-
-
-search.addEventListener("keyup",function(){
-    let text = search.value.toLowerCase();
-    let filtered = products.filter(function(product){
-        return product.title
-        .toLowerCase()
-        .includes(text);
-
-    });
-    displayProducts(filtered);
-});
-
-
-// Task 2 : Category Filter
-
-
-let buttons =document.querySelectorAll(".buttons button");
-buttons.forEach(function(button)
+else 
 {
-button.addEventListener("click",function(){
-let category = button.dataset.category;
-if(category=="all"){
-displayProducts(products);
+
+  console.log("Login Successful: " + username);
+
 }
 
-else
-{
-let filtered =products.filter(function(product){
-return product.category==category;
-});
-
-displayProducts(filtered);
-}
-});
-});
 
 
+// Task ==7 - Website Maintenance
 
-// taask ==3  Price Sorting
-
-
-let lowHigh =document.getElementById("lowHigh");
-let highLow =document.getElementById("highLow");
-lowHigh.addEventListener("click",()=>{
-let sorted=[...products];
-sorted.sort((a,b)=>a.price-b.price);
-displayProducts(sorted);
-});
-highLow.addEventListener("click",()=>{
-let sorted=[...products];
-sorted.sort((a,b)=>b.price-a.price);
-displayProducts(sorted);
-});
+alert("Website is under maintenance.");
+console.error("Error: Website is currently unavailable.");
 
 
+// Task ==8 - Feedback Collection
 
-// task == 4 Product Details Modal
-
-
-
-let modal = document.getElementById("modal");
-let modalBody = document.getElementById("modalBody");
-let closeBtn = document.getElementById("close");
-document.addEventListener("click", function (e) {
-    if (e.target.classList.contains("productImage")) {
-        let id = e.target.dataset.id;
-        let product = products.find(function (item) {
-            return item.id == id;
-        });
-        modal.style.display = "block";
-        modalBody.innerHTML = `
-            <img src="${product.image}" width="200">
-            <h2>${product.title}</h2>
-            <p>${product.description}</p>
-            <h3>Price : $${product.price}</h3>
-            <p><b>Category :</b> ${product.category}</p>
-            <p><b>Rating :</b> ⭐ ${product.rating.rate}</p>
-        `;
-    }
-});
-
-closeBtn.addEventListener("click", function () {
-    modal.style.display = "none";
-});
-window.addEventListener("click", function (e) {
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
-});
+let feedback = prompt("How was today's JavaScript session?");
+console.log("Feedback :", feedback);
+document.writeln("Thank you for your feedback!<br><br>");
 
 
+// Task ==9 - Profile Information
 
-// task =5 == Shopping Cart
+let name = prompt("Enter Your Name:");
+let city = prompt("Enter Your City:");
+let language = prompt("Enter Your Favorite Programming Language:");
+console.log("Name :", name);
+console.log("City :", city);
+console.log("Programming Language :", language);
 
-let cartDiv = document.getElementById("cart");
-let cartCount = document.getElementById("cartCount");
-document.addEventListener("click", function (e) {
-    if (e.target.classList.contains("cartBtn")) {
-        let id = e.target.dataset.id;
-        let product = products.find(function (item) {
-            return item.id == id;
-        });
-        let exist = cart.find(function (item) 
-        {
-            return item.id == id;
-        });
-        if (exist) {
-            exist.quantity++;
-        }
-        else {
-            cart.push({
-                ...product,
-                quantity: 1
-            });
 
-        }
+// Task 10 - Mini Registration Form
 
-        displayCart();
-    }
-});
+let fullName = prompt("Enter Full Name:");
+let email = prompt("Enter Email:");
+let mobile = prompt("Enter Mobile Number:");
+console.log("Registration Details");
+console.log("Name   :", fullName);
+console.log("Email  :", email);
+console.log("Mobile :", mobile);
 
-function displayCart() {
-    cartDiv.innerHTML = "";
-    let grandTotal = 0;
-    cart.forEach(function (item) {
-        let total = item.price * item.quantity;
-        grandTotal += total;
-        cartDiv.innerHTML += `
-            <div class="cartItem">
-                <div>
-                    <h4>${item.title}</h4>
-
-                    <p>Quantity : ${item.quantity}</p>
-                </div>
-                <div>
-                    <h4>$${total.toFixed(2)}</h4>
-                </div>
-            </div>
-
-        `;
-    });
-
-    let totalItems = cart.reduce(function (sum, item) 
-    {
-        return sum + item.quantity;
-    }, 0);
-
-    cartCount.innerHTML = "Items : " + totalItems;
-    cartDiv.innerHTML += `
-        <h2 class="grandTotal">
-
-            Grand Total : $${grandTotal.toFixed(2)}
-
-        </h2>
-    `;
-}
+alert("Registration Successful!");
